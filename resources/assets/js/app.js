86510887d7c627967a65dbe381a48aec980ca032
alekspav <chebrubyonrails@gmail.com>
2019-04-12 15:29:45 +0300

    #1-я задача. Температура
    Настроил шаблон, подключил app.js, app.css, csrf_token, настроил компиляцию компоентов vue.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('temperature', require('./components/temperature/ShowTemperature.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/views/show_temperature/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Температура в Брянске')
+
+
+@section('content')
+<p>Это содержимое тела страницы.</p>
+<temperature></temperature>
+@endsection
+
