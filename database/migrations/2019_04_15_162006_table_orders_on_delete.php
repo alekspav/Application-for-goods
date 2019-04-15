72c73d56227c136d7241eb6c87a9c734786d7aa5
alekspav <chebrubyonrails@gmail.com>
2019-04-15 16:26:49 +0300

    Добавил миграцию для возможности удаления заказов

---
+++ database/migrations/2019_04_15_162006_table_orders_on_delete.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TableOrdersOnDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->dropForeign(['order_id']);
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->dropForeign(['order_id']);
+
+
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+}
